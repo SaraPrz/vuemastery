145c954c235db757bce8bcea179febd40bff39d6
Sara Piroozvand <sarapiroozvand@gmail.com>
2023-07-06 18:54:47 +0330

    solve 1 to 3 tasks of vue mastery

---
+++ dist/js/main.js
@@ -0,0 +1,10 @@
+var app = new Vue ({
+    el : '#app',
+    data: {
+        product : 'socks',
+        description : 'A pair of warm, fuzzy socks',
+        image: './assets/socks.png',
+        href : 'https://v2.vuejs.org/v2/guide/installation.html',
+        onSale : true
+    }
+})
