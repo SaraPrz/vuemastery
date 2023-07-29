21faba753dbf5db63a786d5e1385aafa87d9f8a8
Sara Piroozvand <sarapiroozvand@gmail.com>
2023-07-29 18:23:15 +0330

    done computed properties tasks

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,37 +1,58 @@
 var app = new Vue({
-    el: "#app",
-    data: {
-        product: "socks",
-        description: "A pair of warm, fuzzy socks",
-        image: "./assets/socks.png",
-        href: "https://v2.vuejs.org/v2/guide/installation.html",
-        onSale: true,
-        sizes: ["37", "38", "39", "40"],
-        variants: [
-        {
-            variantId: 2234,
-            variantColor: "green",
-            variantImg: "./assets/socks.png",
-        },
-        {
-            variantId: 2235,
-            variantColor: "blue",
-            variantImg: "./assets/socks-blue.png",
-        },
-        ],
-        cart: 0,
+  el: "#app",
+  data: {
+    product: "socks",
+    brand: "Vue Mastery",
+    description: "A pair of warm, fuzzy socks",
+    href: "https://v2.vuejs.org/v2/guide/installation.html",
+    selectedVariant: 0,
+    sizes: ["37", "38", "39", "40"],
+    onSale: true,
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImg: "./assets/socks.png",
+        variantQuantity: 0,
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImg: "./assets/socks-blue.png",
+        variantQuantity: 10,
+      },
+    ],
+    cart: 0,
+  },
+  methods: {
+    addItem() {
+      this.cart += 1;
     },
-    methods: {
-        addItem() {
-        this.cart += 1;
-        },
-        removeItem() {
-        this.cart -= 1;
-        },
-        updateProduct(variantImg) {
-        this.image = variantImg;
-        },
+    removeItem() {
+      this.cart -= 1;
     },
+    updateProduct(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImg;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    forSale() {
+      if (this.onSale) {
+        return this.brand + ' ' + this.product + ' are on Sale!';
+      }
+      console.log(this.variants.onSale);
+        return this.brand + ' ' + this.product + " are not for Sale!";
+    }
+  },
 });
 
 // var app2 = new Vue({
